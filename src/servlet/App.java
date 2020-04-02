c431d9b53821e3ed13b2d75211b2de4740955359
Riccardo Paltrinieri <61794499+riccardopaltrinieri@users.noreply.github.com>
2020-04-02 15:42:51 +0200

    First Commit

---
+++ src/servlet/App.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package servlet;
+
+/**
+ * @author Paltrinieri Riccardo
+ *
+ */
+public class App {
+
+	/**
+	 * 
+	 */
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
